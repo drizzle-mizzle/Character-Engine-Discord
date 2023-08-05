46d382ccf7a5ca4c864a27a41af8207fecd4ce31
drizzle-mizzle <drizzle-mizzle@hotmail.com>
2023-08-05 13:10:44 +0500

    add swipes toggle

---
+++ Migrations/20230805081014_AddSwipesToggle.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharacterEngineDiscord.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSwipesToggle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SwipesEnabled",
+                table: "CharacterWebhooks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SwipesEnabled",
+                table: "CharacterWebhooks");
+        }
+    }
+}
