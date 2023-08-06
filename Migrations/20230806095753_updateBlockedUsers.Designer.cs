50fad4a6df4ad0d8b61ef8707d38112b8ecbf09c
drizzle-mizzle <drizzle-mizzle@hotmail.com>
2023-08-06 14:58:02 +0500

    migrations

--- Migrations/20230806095753_updateBlockedUsers.Designer.cs
+++ Migrations/20230806095753_updateBlockedUsers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CharacterEngineDiscord.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CharacterEngineDiscord.Migrations
 {
     [DbContext(typeof(StorageContext))]
-    partial class StorageContextModelSnapshot : ModelSnapshot
+    [Migration("20230806095753_updateBlockedUsers")]
+    partial class updateBlockedUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230806095753_updateBlockedUsers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharacterEngineDiscord.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBlockedUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "From",
+                table: "BlockedUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<ulong>(
+                name: "GuildId",
+                table: "BlockedUsers",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Hours",
+                table: "BlockedUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlockedUsers_GuildId",
+                table: "BlockedUsers",
+                column: "GuildId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlockedUsers_Guilds_GuildId",
+                table: "BlockedUsers",
+                column: "GuildId",
+                principalTable: "Guilds",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlockedUsers_Guilds_GuildId",
+                table: "BlockedUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BlockedUsers_GuildId",
+                table: "BlockedUsers");
+
+            migrationBuilder.DropColumn(
+                name: "From",
+                table: "BlockedUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GuildId",
+                table: "BlockedUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Hours",
+                table: "BlockedUsers");
+        }
+    }
+}
