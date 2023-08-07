2006a7089127f2f22692b3de5943ad525609cf2b
drizzle-mizzle <drizzle-mizzle@hotmail.com>
2023-08-07 20:24:52 +0500

    migrations

--- Migrations/20230807152444_fixHunt.Designer.cs
+++ Migrations/20230807152444_fixHunt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CharacterEngineDiscord.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CharacterEngineDiscord.Migrations
 {
     [DbContext(typeof(StorageContext))]
-    partial class StorageContextModelSnapshot : ModelSnapshot
+    [Migration("20230807152444_fixHunt")]
+    partial class fixHunt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230807152444_fixHunt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharacterEngineDiscord.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixHunt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "UserId",
+                table: "HuntedUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0ul);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "HuntedUsers");
+        }
+    }
+}
